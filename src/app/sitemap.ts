9bb13a2bbebde5f57e9f13b0ccab45534d424ccf
moonGoblin <kaungzaw1996@gmail.com>
2024-03-02 03:24:28 +1100

    updated: sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -35,6 +35,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
         '/carpet-cleaning-perth',
         '/window-cleaning-perth',
         '/after-builder-cleaning-perth',
+        '/airbnb-cleaning-perth',
     ].map(route => ({
         url: absoluteUrl(route),
         lastModified: new Date().toISOString(),
